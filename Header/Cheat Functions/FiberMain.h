882222e409924315a09facf90212609243ecf941
HatchesPls <hatchespls@protonmail.com>
2021-10-20 19:17:32 +0200

    Build number to 2.1.0.0

--- Header/Cheat Functions/FiberMain.h
+++ Header/Cheat Functions/FiberMain.h
@@ -6,7 +6,7 @@
 #pragma warning(disable : 4091)	
 #pragma warning(disable : 6262)
 
-#define CHEAT_BUILD_NUMBER "2.0.0.0"
+#define CHEAT_BUILD_NUMBER "2.1.0.0"
 
 #include <windows.h>
 #include <string>
