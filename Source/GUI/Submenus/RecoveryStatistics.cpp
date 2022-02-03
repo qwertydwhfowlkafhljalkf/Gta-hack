9506db6e69d5692eb74926debc9706bff9473cb5
HatchesPls <hatchespls@protonmail.com>
2022-02-03 23:00:50 +0100

    Fixed Max Stealth (Statistics) crashing game (duplicate entry, wrong array index)

--- Source/GUI/Submenus/RecoveryStatistics.cpp
+++ Source/GUI/Submenus/RecoveryStatistics.cpp
@@ -17,32 +17,28 @@ void GUI::Submenus::RecoveryStatistics()
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[0], 100);
 	}
-	if (GUI::Option("Max Stamina", ""))
+	if (GUI::Option("Max Strength", ""))
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[1], 100);
 	}
-	if (GUI::Option("Max Strength", ""))
+	if (GUI::Option("Max Lung Capacity", ""))
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[2], 100);
 	}
-	if (GUI::Option("Max Lung Capacity", ""))
+	if (GUI::Option("Max Driving", ""))
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[3], 100);
 	}
-	if (GUI::Option("Max Driving", ""))
+	if (GUI::Option("Max Flying", ""))
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[4], 100);
 	}
-	if (GUI::Option("Max Flying", ""))
+	if (GUI::Option("Max Shooting", ""))
 	{
 		GameFunctions::SetCharacterSkillStat(SkillStats[5], 100);
 	}
-	if (GUI::Option("Max Shooting", ""))
-	{
-		GameFunctions::SetCharacterSkillStat(SkillStats[6], 100);
-	}
 	if (GUI::Option("Max Stealth", ""))
 	{
-		GameFunctions::SetCharacterSkillStat(SkillStats[7], 100);
+		GameFunctions::SetCharacterSkillStat(SkillStats[6], 100);
 	}
 }
