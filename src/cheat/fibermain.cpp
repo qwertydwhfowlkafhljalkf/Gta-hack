2049a7d219e5c5b21d52f5b891fe9d99b1772091
HatchesPls <hatchespls@protonmail.com>
2022-12-10 13:43:44 +0100

    Removed fibermain capitalization

---
+++ src/cheat/fibermain.cpp
@@ -0,0 +1,12 @@
+﻿#include "fibermain.h"
+
+void Cheat::FiberMain(LPVOID lpParameter)
+{
+	for (;;)
+	{
+		CheatFunctions::Loop();
+		CheatFeatures::Loop();
+		Controls::Loop();
+		GameHooking::PauseMainFiber(0, false);
+	}
+}
