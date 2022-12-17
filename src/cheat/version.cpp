f4ac32c00541f51ff9cc3347016c91e0e8ad74e8
HatchesPls <hatchespls@protonmail.com>
2022-12-17 18:14:00 +0100

    Build number to 2.2.2.0

--- src/cheat/version.cpp
+++ src/cheat/version.cpp
@@ -2,8 +2,8 @@
 
 namespace Cheat
 {
-    const char* build_info::VersionString = "2.2.1.3";
-    const int build_info::VersionInt = 2213;
+    const char* build_info::VersionString = "2.2.2.0";
+    const int build_info::VersionInt = 2200;
     const char* build_info::BuildTime = __TIME__;
     const char* build_info::BuildDate = __DATE__;
 }
