5c2464ae29adb9b8ea20689efd75cdde95ae615c
HatchesPls <hatchespls@protonmail.com>
2022-12-17 18:15:49 +0100

    Updated VersionInt

--- src/cheat/version.cpp
+++ src/cheat/version.cpp
@@ -3,7 +3,7 @@
 namespace Cheat
 {
     const char* build_info::VersionString = "2.2.2.0";
-    const int build_info::VersionInt = 2200;
+    const int build_info::VersionInt = 2220;
     const char* build_info::BuildTime = __TIME__;
     const char* build_info::BuildDate = __DATE__;
 }
